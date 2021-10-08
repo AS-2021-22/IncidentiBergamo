317e12f4920cb8f90b8b95a5b05912711fb112ec
HeyJOe03 <jocarmi03@gmail.com>
2021-10-08 13:16:57 +0200

    deleted data.json

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,16 +1,13 @@
 const express = require('express');
+const mysql = require('mysql')
                 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const data = {
-    
-}
-
 app.get('/',(req,res) => {res.send('hello world')});
 app.post('/data', (req,res) => {
-    res.json(data)
+    res.json({test:'successfull'})
 })
 
 app.listen(PORT,() => console.log(`>Server listening on PORT: ${PORT}`))
