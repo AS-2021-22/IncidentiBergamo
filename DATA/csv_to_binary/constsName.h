b8ec11e30b4a963048292c22584fee78ac9b2838
HeyJOe03 <jocarmi03@gmail.com>
2021-10-13 11:58:17 +0200

    started c++ project

---
+++ DATA/csv_to_binary/constsName.h
@@ -0,0 +1,21 @@
+#ifndef CONSTS_FIELDS
+#define CONSTS_FIELDS
+
+#define Protocollo 0
+#define Anno 1
+#define Data 2
+#define Ora 3
+#define Localita 4
+#define NaturaIncidente 5
+#define Illesi 6
+#define Feriti 7
+#define Riservata 8
+#define Morti 9
+#define Pedoni 10
+#define Velocipedi 11
+#define Ciclomotori 12
+#define MezziPesanti 13
+#define LocalizzazioneX 14
+#define LocalizzazioneY 15
+
+#endif
---
+++ DATA/csv_to_binary/csv_to_binary.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "constsName.h"
+
+#define NOME_FILE_CSV_INPUT "./../incidenti_bergamo_short.csv"
+
+using namespace std;
+
+void stringSplitter(vector<string>& v,string s){
+    int offset = 0;
+    string field = "";
+    bool eof = false;
+    while(eof == false){
+        int pos = s.find(',',offset);
+        if(pos == -1) {eof = true; pos = s.size()-1;}
+        field = s.substr(offset,pos-offset);
+        offset = pos + 1;
+        v.push_back(field);
+    }
+}
+
+void read(){
+    fstream CSVreader(NOME_FILE_CSV_INPUT, ios::in | ios::binary);
+    if(CSVreader.is_open()){
+        string row;
+        while(!CSVreader.eof()){
+            vector<string> rowV;
+            getline(CSVreader,row);
+            stringSplitter(rowV,row);
+        }
+    }
+    else cout<<"error"<<endl;
+}
+
+
+int main(){
+    read();
+    return 0;
+}
