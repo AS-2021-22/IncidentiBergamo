3fb5a65be13606de883f83e0f38eff15698735ef
Scotti Davide <Scotti.17115>
2021-10-14 10:53:25 +0200

    added PoupUp

--- map-incidenti-bergamo/src/models/MapElement.jsx
+++ map-incidenti-bergamo/src/models/MapElement.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import '../style/App.css'
+import {PopUp} from './PopUp'
 //import 'leaflet/dist/leaflet.css' //this create some problems with the marker, so it is imported in the html file
-
+const coordBergamo = [45.6982642, 9.6772698]
 
 function stringCoordParser(sc){
     let [X,Y] = sc.substring(1,sc.length - 2).split(',')
@@ -12,7 +13,7 @@ function stringCoordParser(sc){
 export default function MapElement({points}) {
     return (
         <div>
-            <MapContainer style={{height:'80vh'}} center={[45.6982642, 9.6772698]} zoom={13} scrollWheelZoom={false}>
+            <MapContainer style={{height:'80vh'}} center={coordBergamo} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -22,12 +23,16 @@ export default function MapElement({points}) {
                         return (
                             <Marker key={point.Protocollo} position={stringCoordParser(point.Localizzazione)}>
                                 <Popup>
-                                    {point.Protocollo}
+                                    <h1>
+                                        {point.Protocollo}
+                                    </h1>
                                 </Popup>
                             </Marker>
                         )
                     })
                 }
+
+                <PopUp incidente={{id:'scrivigli',eta:4}}/>
             </MapContainer>
         </div>
     )
